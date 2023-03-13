965e7c952e64652ea7dab22786f15524b546e3cb
ryanspurrell <rspurrell2@gmail.com>
2023-03-12 18:24:45 -0600

    Implemented Parcelable to the QRCode class Created CustomHeader class, to display a header with a back button and text on every page (not working atm) Added the custom header to some of the activities (not all of them yet) Added attrs.xml file

--- app/src/main/java/com/example/qrazy/MapActivity.java
+++ app/src/main/java/com/example/qrazy/MapActivity.java
@@ -1,21 +1,18 @@
 package com.example.qrazy;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
-import android.Manifest;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
@@ -23,7 +20,6 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
-import org.osmdroid.views.overlay.ItemizedOverlayControlView;
 import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
 import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
@@ -39,7 +35,6 @@ public class MapActivity extends AppCompatActivity {
     private MapView map = null;
     private final int REQUEST_PERMS = 1;
     Context context;
-    private MyLocationNewOverlay mLocationOverlay;
 
     /**
      * onCreate function
@@ -54,7 +49,21 @@ public class MapActivity extends AppCompatActivity {
         Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context));
         setContentView(R.layout.activity_map);
 
-        map = (MapView) findViewById(R.id.map);
+        // get the arguments passed by the intent
+        String last_act = getIntent().getStringExtra("activityName");
+
+        // create the header
+        CustomHeader head = findViewById(R.id.header_qr_leaderboard);
+        head.initializeHead("Map", "Back to " + last_act);
+        // set listener for back button in the header
+        // TODO: button needs to be clicked multiple times to work, need to fix that
+        head.back_button.setOnClickListener(view -> {
+            Log.d("Back button","Back button clicked");
+            finish();
+        });
+
+        // get the map view
+        map = findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
 
         // these are for permissions needed to make the map work properly
@@ -70,9 +79,9 @@ public class MapActivity extends AppCompatActivity {
         GeoPoint start = new GeoPoint(lat,lon);
 
         // creating an overlay for the map
-        this.mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(getBaseContext()),map);
-        this.mLocationOverlay.enableMyLocation();
-        map.getOverlays().add(this.mLocationOverlay);
+        MyLocationNewOverlay mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(getBaseContext()), map);
+        mLocationOverlay.enableMyLocation();
+        map.getOverlays().add(mLocationOverlay);
 
         requestPermissions(permsArray);
 
@@ -81,21 +90,10 @@ public class MapActivity extends AppCompatActivity {
         mapController.setCenter(start);
 
         // want to create some dummy points, to make sure the map can actually show them
-        ArrayList<OverlayItem> points = new ArrayList<OverlayItem>();
+        ArrayList<OverlayItem> points = new ArrayList<>();
         points.add(new OverlayItem("Test","Test description",start));
 
         makeMapMarker(points);
-
-        // make the back button finish the activity, and set label on back button
-        TextView back_button_label = findViewById(R.id.back_button_label);
-        ImageButton back_button = findViewById(R.id.back_button_map);
-        String last_activity = getIntent().getStringExtra("activityName");
-        back_button_label.setText("Back to " + last_activity);
-
-        // go back to the last activity
-        back_button.setOnClickListener(view -> finish());
-
-
     }
 
     @Override
@@ -151,7 +149,7 @@ public class MapActivity extends AppCompatActivity {
      */
     private void makeMapMarker(ArrayList<OverlayItem> points) {
         // TODO: deal with deprecated classes from Open Maps
-        ItemizedOverlayWithFocus<OverlayItem> overlay = new ItemizedOverlayWithFocus<OverlayItem>(points,
+        ItemizedOverlayWithFocus<OverlayItem> overlay = new ItemizedOverlayWithFocus<>(points,
                 new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
                     @Override
                     public boolean onItemSingleTapUp(int index, OverlayItem item) {
@@ -184,14 +182,11 @@ public class MapActivity extends AppCompatActivity {
         new AlertDialog.Builder(MapActivity.this)
                 .setTitle("Test")
                 .setMessage("Would you like to go to the leaderboard for this QR code?")
-                .setPositiveButton("Go to leaderboard", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Intent intent = new Intent(getBaseContext(), activityClass);
-                        intent.putExtra("prevActivity","map");
-                        intent.putExtra("QRCode",qrCode);
-                        startActivity(intent);
-                    }
+                .setPositiveButton("Go to leaderboard", (dialogInterface, i) -> {
+                    Intent intent = new Intent(getBaseContext(), activityClass);
+                    intent.putExtra("prevActivity","map");
+                    intent.putExtra("QRCode",qrCode);
+                    startActivity(intent);
                 })
                 .setNegativeButton("Cancel",null)
                 .show();
--- app/src/main/java/com/example/qrazy/QRCode.java
+++ app/src/main/java/com/example/qrazy/QRCode.java
@@ -2,20 +2,43 @@ package com.example.qrazy;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.util.Pair;
+
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Class to represent qr codes
+ * Implements Parcelable so that a QRCode can be passed between activities with intent.setExtra
  */
 public class QRCode implements Parcelable {
 
-    private String label;
+    private String content;
+    private String name;
+    private int score;
+    private Pair<Double, Double> location;  // <longitude, latitude>
+    private HashMap<String, String> comments;  // <userID, comment>
 
-    /**
-     * Constructor method
-     */
-    public QRCode(String label) {
-        this.label = label;
+    public QRCode(String content) {
+        this.content = content;
     }
+
+    protected QRCode(Parcel in) {
+        this.content = in.readString();
+    }
+
+    public static final Creator<QRCode> CREATOR = new Creator<QRCode>() {
+        @Override
+        public QRCode createFromParcel(Parcel in) {
+            return new QRCode(in);
+        }
+
+        @Override
+        public QRCode[] newArray(int size) {
+            return new QRCode[size];
+        }
+    };
 
     @Override
     public int describeContents() {
@@ -23,16 +46,36 @@ public class QRCode implements Parcelable {
     }
 
     @Override
-    public void writeToParcel(Parcel parcel, int number) {}
-
-    public void setLabel(String label) {
-        this.label = label;
+    public void writeToParcel(Parcel parcel, int number) {
+        parcel.writeString(content);
     }
 
-    public String getLabel() {
-        return this.label;
+    public int updateScore() {
+
+        int score = 0;
+        String hash = getHash();
+        // regex global search for substrings of repeating chars
+        Pattern repeat = Pattern.compile("(.)(\\1+)");
+        Matcher matcher = repeat.matcher(hash);
+        while(matcher.find()) {
+            String match = matcher.group();
+            // TODO <- map hex value to decimal; 0 to 20
+            int char_val = 10;
+            score += Math.pow(char_val,(match.length()-1));
+        }
+        // update and ret
+        this.score = score;
+        return score;
     }
 
+    public String getHash() {
+        return org.apache.commons.codec.digest.DigestUtils.sha256Hex(content);
+    }
 
-
+    public int getScore() {
+        return score;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
